6adcc97ffe9bfa1d671165b40951354129d16128
Matvysha <rhade620@gmail.com>
2024-06-27 11:25:36 +0300

    second commit

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: 'Chill Box - Suzume no Tojimari tiktok remix (Nightcore)',
+        artist: 'Chill Box ',
+        img: 'Sudzume',
+        src: 'music-1'
+    },
+    {
+        name: 'YOASOBI - IDOL',
+        artist: 'YOASOBI',
+        img: 'AI',
+        src: 'music-2'
+    },
+    {
+        name: 'Nasuo - Honey Jet Coaster',
+        artist: 'Nasuo',
+        img: 'Shikimori',
+        src: 'music-3'
+    }, 
+    {
+        name: 'Ziyoou-vachi - Mephisto (Oshi no Ko ED)',
+        artist: 'Ziyoou-vachi ',
+        img: 'Aqua',
+        src: 'music-4'
+    },
+    {
+        name: 'KENTENSHI - paranoia',
+        artist: 'KENTENSHI',
+        img: 'paranoia',
+        src: 'music-5'
+    }
+]
